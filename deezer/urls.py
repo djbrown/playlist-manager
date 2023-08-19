d737b4c9b3d9beb520fcde57de52c13ff3f78998
Daniel Brown <djbrown@users.noreply.github.com>
2023-08-19 10:11:23 +0200

    add views

---
+++ deezer/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("callback/", views.callback, name="callback"),
+    path("playlists/", views.playlists, name="playlists"),
+]
--- deezer/views.py
+++ deezer/views.py
@@ -1,3 +1,45 @@
+import requests
+from django.conf import settings
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Playlist
+
+
+def index(request):
+    return render(request, "deezer/index.html")
+
+
+def login(request: HttpRequest):
+    context = {
+        "DEEZER_APP_ID": settings.DEEZER_APP_ID,
+        "DEEZER_CALLBACK_URL": settings.DEEZER_CALLBACK_URL,
+    }
+    return render(request, "deezer/login.html", context)
+
+
+def callback(request: HttpRequest):
+    code = request.GET.get("code")
+
+    token_response = requests.get(
+        "https://connect.deezer.com/oauth/access_token.php",
+        params={
+            "app_id": settings.DEEZER_APP_ID,
+            "secret": settings.DEEZER_SECRET_KEY,
+            "code": code,
+            "output": "json",
+        },
+    ).json()
+
+    response = HttpResponseRedirect("/deezer/")
+    response.set_cookie(
+        "deezer_access_token",
+        token_response["access_token"],
+        max_age=token_response["expires"],
+    )
+    return response
+
+
+def playlists(request: HttpRequest):
+    context = {"playlists": Playlist.objects.all()}
+    return render(request, "deezer/playlists.html", context)

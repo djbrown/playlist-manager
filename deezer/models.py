23a50e508ccdd00f2c6f1167a9742e29d191152d
Daniel Brown <djbrown@users.noreply.github.com>
2023-08-19 09:59:22 +0200

    add model

--- deezer/models.py
+++ deezer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Track(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=255)
+    tracks = models.ManyToManyField(Track)
+    link = models.URLField()
